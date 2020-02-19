042c6bbefaae0070932f0974bf4f31bbdc6d9e57
agnew832 <agnew832@gmail.com>
2020-02-19 11:04:48 -0500

    final commit

---
+++ src/app/post-form/post-form.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+@Component({
+  selector: "app-post-form",
+  templateUrl: "./post-form.component.html",
+  styleUrls: ["./post-form.component.css"]
+})
+export class PostFormComponent implements OnInit {
+  @Output() submitted = new EventEmitter<any>();
+  constructor() {}
+
+  ngOnInit(): void {}
+  addPost(form: NgForm) {
+    console.log(form);
+    this.submitted.emit(form.value);
+  }
+}
